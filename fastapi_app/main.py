413d04386a1148926b369c02dfcfa6504a6707cd
Artur Siedlecki <asiedlecki@protonmail.com>
2026-06-24 08:26:23 +0200

    Refactor of the fetching method to reduce duplicated code.

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -49,58 +49,32 @@ async def fetch_similar_stores_data(
             query_store = await connection.fetchrow(QUERY_STORE_DETAILS_QUERY, store_id)
             sim_stores = await connection.fetch(query, store_id, n)
 
+            columns = [
+                "kpi_footfall", "kpi_basket_size", "kpi_conversion_rate", "kpi_transactions",
+                "kpi_revenue", "kpi_margin_rate", "kpi_margin", "kpi_competition_score",
+                "kpi_mall_attractiveness_score", "sm_pieczywo", "sm_warzywa_i_owoce",
+                "sm_slodycze_i_slone_przekaski", "sm_piwo", "sm_alkohole_mocne",
+                "sm_papierosy", "sm_fast_food", "sm_woda_i_napoje_niealkoholowe", "sm_sery_i_wedliny"
+            ]
+
             return {
-    "data": {
-        "query_store": {
-            "store_id": query_store["store_id"],
-            "city": query_store["city_name"],
-            "population": query_store["population"],
-            "kpi_footfall": query_store["kpi_footfall"],
-            "kpi_basket_size": query_store["kpi_basket_size"],
-            "kpi_conversion_rate": query_store["kpi_conversion_rate"],
-            "kpi_transactions": query_store["kpi_transactions"],
-            "kpi_revenue": query_store["kpi_revenue"],
-            "kpi_margin_rate": query_store["kpi_margin_rate"],
-            "kpi_margin": query_store["kpi_margin"],
-            "kpi_competition_score": query_store["kpi_competition_score"],
-            "kpi_mall_attractiveness_score": query_store["kpi_mall_attractiveness_score"],
-            "sm_pieczywo": query_store["sm_pieczywo"],
-            "sm_warzywa_i_owoce": query_store["sm_warzywa_i_owoce"],
-            "sm_slodycze_i_slone_przekaski": query_store["sm_slodycze_i_slone_przekaski"],
-            "sm_piwo": query_store["sm_piwo"],
-            "sm_alkohole_mocne": query_store["sm_alkohole_mocne"],
-            "sm_papierosy": query_store["sm_papierosy"],
-            "sm_fast_food": query_store["sm_fast_food"],
-            "sm_woda_i_napoje_niealkoholowe": query_store["sm_woda_i_napoje_niealkoholowe"],
-            "sm_sery_i_wedliny": query_store["sm_sery_i_wedliny"]
-        },
-        "results": [
-            {
-                "store_id": store["store_id"],
-                "city": store["city_name"],
-                "kpi_footfall": store["kpi_footfall"],
-                "kpi_basket_size": store["kpi_basket_size"],
-                "kpi_conversion_rate": store["kpi_conversion_rate"],
-                "kpi_transactions": store["kpi_transactions"],
-                "kpi_revenue": store["kpi_revenue"],
-                "kpi_margin_rate": store["kpi_margin_rate"],
-                "kpi_margin": store["kpi_margin"],
-                "kpi_competition_score": store["kpi_competition_score"],
-                "kpi_mall_attractiveness_score": store["kpi_mall_attractiveness_score"],
-                "sm_pieczywo": store["sm_pieczywo"],
-                "sm_warzywa_i_owoce": store["sm_warzywa_i_owoce"],
-                "sm_slodycze_i_slone_przekaski": store["sm_slodycze_i_slone_przekaski"],
-                "sm_piwo": store["sm_piwo"],
-                "sm_alkohole_mocne": store["sm_alkohole_mocne"],
-                "sm_papierosy": store["sm_papierosy"],
-                "sm_fast_food": store["sm_fast_food"],
-                "sm_woda_i_napoje_niealkoholowe": store["sm_woda_i_napoje_niealkoholowe"],
-                "sm_sery_i_wedliny": store["sm_sery_i_wedliny"],
-                "similarity": store["similarity"],
-            } for store in sim_stores
-        ]
-    }
-}
+                "data": {
+                    "query_store": {
+                        "store_id": query_store["store_id"],
+                        "city": query_store["city_name"],
+                        "population": query_store["population"],
+                        **{col: query_store[col] for col in columns}
+                    },
+                    "results": [
+                        {
+                            "store_id": store["store_id"],
+                            "city": store["city_name"],
+                            "similarity": store["similarity"],
+                            **{col: query_store[col] for col in columns}
+                        } for store in sim_stores
+                    ]
+                }
+            }
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Błąd podczas obliczania podobieństwa v1: {str(e)}")
 
